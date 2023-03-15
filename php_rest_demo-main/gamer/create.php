4a1f9969909e9d2aab9cedb9c1fe14bad5a45d75
lai2-01 <lai2-01@itismeucci.com>
2023-03-15 09:39:46 +0100

    primo commit

---
+++ php_rest_demo-main/gamer/create.php
@@ -0,0 +1,38 @@
+<?php
+
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+// include database and object files
+include_once '../config/db.php';
+include_once '../object/Gamer.php';
+
+$database = new Db();
+$db = $database->getConnection();
+
+// initialize object
+$gamer = new Gamer($db);
+
+// get posted data
+$data = json_decode(file_get_contents("php://input", true));
+
+// set department property value
+$gamer->name = $data->name;
+
+// create the department
+if ($gamer->create()) {
+    echo '{';
+    echo '"message": "gamer was created."';
+    echo '}';
+}
+
+// if unable to create the department, tell the user
+else {
+    echo '{';
+    echo '"message": "Unable to create gamer."';
+    echo '}';
+}
---
+++ php_rest_demo-main/gamer/update.php
@@ -0,0 +1,40 @@
+<?php
+
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: PUT");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+// include database and object files
+include_once '../config/db.php';
+include_once '../object/Gamer.php';
+
+$database = new Db();
+$db = $database->getConnection();
+
+// initialize object
+$gamer = new Department($db);
+
+// get posted data
+$data = json_decode(file_get_contents("php://input", true));
+
+// set ID property of department to be updated
+$gamer->id = $data->id;
+// set department property value
+$gamer->name = $data->name;
+
+// update the department
+if ($department->update()) {
+    echo '{';
+    echo '"message": "Gamer was updated."';
+    echo '}';
+}
+
+// if unable to update the department, tell the user
+else {
+    echo '{';
+    echo '"message": "Unable to update gamer."';
+    echo '}';
+}
